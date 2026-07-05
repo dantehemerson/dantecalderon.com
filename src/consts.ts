8f8ff7059dd77095328e78d30a3e67c2f117e656
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2026-07-05 01:56:10 -0500

    feat: optimize substack s3 image urls

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,1 @@
-export const IMAGE_DOMAINS = ['substackcdn.com', 'dantecalderon.com', 'localhost']
+export const IMAGE_DOMAINS = ['substackcdn.com', 'dantecalderon.com', 'substack-post-media.s3.amazonaws.com', 'localhost']
